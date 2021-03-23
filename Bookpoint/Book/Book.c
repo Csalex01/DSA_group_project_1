634d290003b2a68dfb71d25420d1fab1f9bc85a0
Csillag Alex <csillagalex2001@gmail.com>
2021-03-23 15:15:40 +0200

    Create and destroy functions for each struct

--- Bookpoint/Book/Book.c
+++ Bookpoint/Book/Book.c
@@ -1,5 +1,30 @@
-//
-// Created by csill on 3/21/2021.
-//
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "Book.h"
+
+Book* createBook(char* ISBN, char* title, float price, int numberOfPages, Publisher publisher, Date publishDate, Person author, enum Cover cover, bool ebook){
+    Book* book = (Book*)malloc(sizeof(Book));
+
+    if(!book) {
+        printf("Failed to allocate memory for Book");
+        return NULL;
+    }
+
+    book->ISBN = ISBN;
+    book->title = title;
+    book->price = price;
+    book->numberOfPages = numberOfPages;
+    book->publisher = publisher;
+    book->publishDate = publishDate;
+    book->author = author;
+    book->cover = cover;
+    book->ebook = ebook;
+
+    return book;
+}
+
+void destroyBook(Book* book) {
+    free(book);
+    book = NULL;
+}
--- Bookpoint/Date/Date.c
+++ Bookpoint/Date/Date.c
@@ -1,5 +1,24 @@
-//
-// Created by csill on 3/21/2021.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "Date.h"
+
+Date* createDate(int year, int month, int day) {
+    Date* date = (Date*)malloc(sizeof(Date));
+
+    if(!date) {
+        printf("Failed to allocate memory for Date");
+        return NULL;
+    }
+
+    date->year = year;
+    date->month = month;
+    date->day = day;
+
+    return date;
+}
+
+void destroyDate(Date* date) {
+    free(date);
+    date = NULL;
+}
